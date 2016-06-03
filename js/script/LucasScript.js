127e6ec36c42986ce000c653442ca9c8c8dcd171
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2016-06-03 13:26:53 +0200

    push

--- js/script/LucasScript.js
+++ js/script/LucasScript.js
@@ -532,7 +532,7 @@ var LucasScript = {
         var meshLoop = kobbelt.launchKobbelt(); 
 
 
-        var materialObject = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading, side: THREE.doubleSide} );
+        var materialObject = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading, side: THREE.DoubleSide} );
         tw.scenes.main.add(meshLoop.buildThreeMesh(materialObject));    
 
     },
--- js/script/main.js
+++ js/script/main.js
@@ -4,7 +4,7 @@ $(document).ready(function()  {
 		LucasScript,
 		//ThomasScript,
 		KevinScript,
-		GuillaumeScript
+		//GuillaumeScript
 	];
 
 	//*
